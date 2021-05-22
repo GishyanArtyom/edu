e0cf0a0afcba6a8e942f97ccb3139206fbd7ecd0
GishyanArtyom <arthades047@gmail.com>
2021-05-22 14:10:10 +0400

    Create find4octnum.sh

---
+++ find4octnum.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Find any sequence like number.number.number.number template from given ARGUMNET
+# Must invoke find4octnum.sh FILE
+
+egrep  --color=auto '([0-9]+\.){3}[0-9]' $1 | egrep -v '([0-9]+\.){5,}'
