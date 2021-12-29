f317872bdd232d3abfa6f13a2e5d9cf598c07767
GishyanArtyom <arthades047@gmail.com>
2021-12-29 10:19:00 +0400

    Delete ChallengeLabs directory

--- ChallengeLabs/lab1/uniquservices.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# There thow cut command is for normal script behavior in different bash versions.
-grep '^[A-Za-z]' /etc/services | cut -d' ' -f1 | cut -f1 | sort --unique > $HOME/uniqueservices.txt && nl $HOME/uniqueservices.txt
